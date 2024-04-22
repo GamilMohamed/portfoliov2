d58ea176793141f19e209bc610f7e2f04dbd2504
mohazerro <mohazerro@github.com>
2024-04-22 15:13:19 +0200

    resume

--- src/Languages/AboutMe.tsx
+++ src/Languages/AboutMe.tsx
@@ -49,7 +49,7 @@ function AboutMe() {
                 button={"READ"}
                 title={"CV"}
                 // action={() => (location.href = "src/assets/resume.pdf")}
-                href="src/assets/resume.pdf"
+                href="assets/resume.pdf"
               >
                 <>
                   <p className="textbigscreen">Click to see my CV</p>
